23c651723418a15f270c45129938e184f0e8034f
Kevin Mehall <km@kevinmehall.net>
2010-08-07 14:46:34 -0600

    Update notify plugin:   * Use pithos logo for fallback icon   * Show notification for play/pause media keys (Fixes bug #614821)

--- pithos/song_notify.py
+++ pithos/song_notify.py
@@ -15,8 +15,9 @@
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
-import pynotify
+import pynotify, gtk
 from pithos.plugin import PithosPlugin
+from pithosconfig import get_data_file
 
 class NotifyPlugin(PithosPlugin):
 	def on_prepare(self):
@@ -25,15 +26,33 @@ class NotifyPlugin(PithosPlugin):
 		
 	def on_enable(self):
 		self.song_callback_handle = self.window.connect("song-changed", self.song_changed)
+		self.state_changed_handle = self.window.connect("user-changed-play-state", self.playstate_changed)
+		
+    def set_for_song(self, song):
+        msg = "by %s from %s"%(song.artist, song.album)
+	    self.notification.update(song.title, msg, 'audio-x-generic')
 		
 	def song_changed(self, window,  song):
 		if not self.window.is_active():
-			msg = "by %s from %s"%(song.artist, song.album)
-			self.notification.update(song.title, msg, "audio-x-generic")
+			self.set_for_song(song)
 			if song.art_pixbuf:
 			    #logging.debug("has albumart", song.art_pixbuf, song.art_pixbuf.get_width())
 			    self.notification.set_icon_from_pixbuf(song.art_pixbuf)
+			else:
+			    self.notification.props.icon_name = get_data_file('media/pithos-mono.png')
 			self.notification.show()
+			
+    def playstate_changed(self, window, state):
+        if not self.window.is_active():
+            self.set_for_song(window.current_song)
+            if state:
+                self.notification.props.icon_name = 'gtk-media-play-ltr'
+            else:
+                self.notification.props.icon_name = 'gtk-media-pause'
+            
+            self.notification.show()
+            
 		
 	def on_disable(self):
 		self.window.disconnect(self.song_callback_handle)
+		self.window.disconnect(self.state_changed_handle)
