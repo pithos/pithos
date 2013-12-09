5c23b3134b23f00890591c47a626bfb3b6ae73de
obadz <dav-github@odav.org>
2013-12-09 10:47:58 +0000

    Scroll over notification icon to adjust volume.
    Only supported on some platforms.

--- pithos/plugins/notification_icon.py
+++ pithos/plugins/notification_icon.py
@@ -48,7 +48,18 @@ class PithosNotificationIcon(PithosPlugin):
             self.statusicon.connect('activate', self.toggle_visible)
         
         self.build_context_menu()
-       
+
+    def scroll(self, steps):
+        if indicator_capable:
+            direction = steps.value_nick
+        else:
+            direction = steps.direction.value_nick
+
+        if direction == 'down':
+            self.window.adjust_volume(-1)
+        elif direction == 'up':
+            self.window.adjust_volume(+1)
+
     def build_context_menu(self):
         menu = Gtk.Menu()
         
@@ -80,9 +91,12 @@ class PithosNotificationIcon(PithosPlugin):
         # connect our new menu to the statusicon or the appindicator
         if indicator_capable:
             self.ind.set_menu(menu)
+            # Disabled because of https://bugs.launchpad.net/variety/+bug/1071598
+            #self.ind.connect('scroll-event', lambda _x, _y, steps: self.scroll(steps))
         else:
             self.statusicon.connect('popup-menu', self.context_menu, menu)
-            
+            self.statusicon.connect('scroll-event', lambda _, steps: self.scroll(steps))
+
         self.menu = menu
 
 
