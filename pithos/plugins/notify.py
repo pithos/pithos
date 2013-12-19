d92371c7efc3e417bf1a5cfb6d8c4b1e36c9d4de
TingPing <tingping@tingping.se>
2013-12-18 19:46:34 -0500

    Show play/pause and skip actions in notifications

--- pithos/plugins/notify.py
+++ pithos/plugins/notify.py
@@ -14,47 +14,86 @@
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
+import logging
 from cgi import escape
 from pithos.plugin import PithosPlugin
 from pithos.pithosconfig import get_data_file
-from gi.repository import Notify
+from gi.repository import (GLib, Gtk, Notify)
 
 class NotifyPlugin(PithosPlugin):
     preference = 'notify'
+
+    supports_actions = False
     
     def on_prepare(self):
-        Notify.init('pithos')
+        Notify.init('Pithos')
         self.notification = Notify.Notification()
-        
+        self.notification.set_category('x-gnome.music')
+        self.notification.set_hint_string('desktop-icon', 'pithos')
+
+        caps = Notify.get_server_caps()
+        if 'actions' in caps:
+            logging.info('Notify supports actions')
+            self.supports_actions = True
+
+        if 'action-icons' in caps:
+            self.notification.set_hint('action-icons', GLib.Variant.new_boolean(True))
+
+        # TODO: On gnome this can replace the tray icon, just need to add love/hate buttons
+        #if 'persistence' in caps:
+        #    self.notification.set_hint('resident', GLib.Variant.new_boolean(True))
+
     def on_enable(self):
         self.song_callback_handle = self.window.connect("song-changed", self.song_changed)
         self.state_changed_handle = self.window.connect("user-changed-play-state", self.playstate_changed)
+
+    def set_actions(self, playing=True):
+        self.notification.clear_actions()
+
+        pause_action = 'media-playback-pause'
+        play_action = 'media-playback-start'
+        skip_action = 'media-skip-forward'
+
+        if Gtk.Widget.get_default_direction() == Gtk.TextDirection.RTL:
+            play_action += '-rtl'
+            skip_action += '-rtl'
+
+        if playing:
+            self.notification.add_action(pause_action, 'Pause',
+                                         self.notification_playpause_cb, None)
+        else:
+            self.notification.add_action(play_action, 'Play',
+                                         self.notification_playpause_cb, None)
+
+        self.notification.add_action(skip_action, 'Skip',
+                                     self.notification_skip_cb, None)
+
+    def set_notification(self, song, playing=True):
+        if self.supports_actions:
+            self.set_actions(playing)
+
+        if song.art_pixbuf:
+            self.notification.set_image_from_pixbuf(song.art_pixbuf)
+        else:
+            self.notification.set_hint('image-data', None)
         
-    def set_for_song(self, song):
-        self.notification.clear_hints()
-        msg = escape("by %s from %s"%(song.artist, song.album))
+        msg = escape('by {} from {}'.format(song.artist, song.album))
         self.notification.update(song.title, msg, 'audio-x-generic')
+        self.notification.show()
+
+    def notification_playpause_cb(self, notification, action, data):
+        self.window.playpause_notify()
+
+    def notification_skip_cb(self, notification, action, data):
+        self.window.next_song()
         
     def song_changed(self, window,  song):
         if not self.window.is_active():
-            self.set_for_song(song)
-            if song.art_pixbuf:
-                #logging.debug("has albumart", song.art_pixbuf, song.art_pixbuf.get_width())
-                self.notification.set_icon_from_pixbuf(song.art_pixbuf)
-            else:
-                self.notification.props.icon_name = get_data_file('media/pithos-mono.png')
-            self.notification.show()
+            GLib.idle_add(self.set_notification, window.current_song)
             
     def playstate_changed(self, window, state):
         if not self.window.is_active():
-            self.set_for_song(window.current_song)
-            if state:
-                self.notification.props.icon_name = 'gtk-media-play-ltr'
-            else:
-                self.notification.props.icon_name = 'gtk-media-pause'
-            
-            self.notification.show()
-            
+            GLib.idle_add(self.set_notification, window.current_song, state)
         
     def on_disable(self):
         self.window.disconnect(self.song_callback_handle)
