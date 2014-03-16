cb10d9246a94eef6b025059de031e7137dc6891d
Greg Sheremeta <gshereme@redhat.com>
2014-03-16 14:53:09 -0400

    added new tray icon to setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ def read(fname):
 if sys.platform != 'win32':
     data_files = [
         ('/usr/share/icons/hicolor/scalable/apps', ['data/icons/hicolor/pithos.svg']),
+        ('/usr/share/icons/hicolor/48x48/apps', ['data/icons/hicolor/pithos-tray-icon.png']),
         ('/usr/share/icons/ubuntu-mono-dark/apps/16', ['data/icons/ubuntu-mono-dark/pithos-tray-icon.svg']),
         ('/usr/share/icons/ubuntu-mono-light/apps/16', ['data/icons/ubuntu-mono-light/pithos-tray-icon.svg']),
         ('/usr/share/applications', ['data/pithos.desktop'])
