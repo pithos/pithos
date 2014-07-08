b867fbd530ff2cec4ad3a130922a56d6f9c46c8c
Kyle Kinkade <kkinkade@wetpaint-inc.com>
2014-07-08 11:49:25 -0700

    adding support for storing files w/ & and +

--- pithos/pandora/pandora.py
+++ pithos/pandora/pandora.py
@@ -446,7 +446,7 @@ class Song(object):
         self.delete_temp()
 
     def make_safe(self, filename):
-        valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+        valid_chars = "&+-_.() %s%s" % (string.ascii_letters, string.digits)
         return ''.join(c for c in filename if c in valid_chars)
 
     @property
