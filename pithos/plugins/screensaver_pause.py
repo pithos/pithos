0b43c7a59a8899046c3d37d55ea523f0cdf91b02
Glenn Moss <gim@keynetics.com>
2014-08-07 16:55:33 -0600

    Locking/unlocking screen in Unity now handled by screensaver_pause

--- pithos/plugins/screensaver_pause.py
+++ pithos/plugins/screensaver_pause.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: nil; -*-
 ### BEGIN LICENSE
 # Copyright (C) 2010-2012 Kevin Mehall <km@kevinmehall.net>
-#This program is free software: you can redistribute it and/or modify it 
-#under the terms of the GNU General Public License version 3, as published 
+#This program is free software: you can redistribute it and/or modify it
+#under the terms of the GNU General Public License version 3, as published
 #by the Free Software Foundation.
 #
-#This program is distributed in the hope that it will be useful, but 
-#WITHOUT ANY WARRANTY; without even the implied warranties of 
-#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranties of
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 #PURPOSE.  See the GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License along 
+#You should have received a copy of the GNU General Public License along
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -22,7 +22,7 @@ class ScreenSaverPausePlugin(PithosPlugin):
     preference = 'enable_screensaverpause'
 
     session_bus = None
-    
+
     def bind_session_bus(self):
         try:
             import dbus
@@ -34,7 +34,7 @@ class ScreenSaverPausePlugin(PithosPlugin):
             return True
         except dbus.DBusException:
             return False
-        
+
     def on_enable(self):
         if not self.bind_session_bus():
             logging.error("Could not bind session bus")
@@ -52,6 +52,8 @@ class ScreenSaverPausePlugin(PithosPlugin):
             self.session_bus.add_signal_receiver(self.playPause, 'ActiveChanged', 'org.gnome.ScreenSaver')
             self.session_bus.add_signal_receiver(self.playPause, 'ActiveChanged', 'org.cinnamon.ScreenSaver')
             self.session_bus.add_signal_receiver(self.playPause, 'ActiveChanged', 'org.freedesktop.ScreenSaver')
+            self.session_bus.add_signal_receiver(self.pause, 'Locked', 'com.canonical.Unity.Session')
+            self.session_bus.add_signal_receiver(self.play, 'Unlocked', 'com.canonical.Unity.Session')
             return True
         except dbus.DBusException:
             logging.info("Enable failed")
@@ -62,11 +64,18 @@ class ScreenSaverPausePlugin(PithosPlugin):
             self.session_bus.remove_signal_receiver(self.playPause, 'ActiveChanged', 'org.gnome.ScreenSaver')
             self.session_bus.remove_signal_receiver(self.playPause, 'ActiveChanged', 'org.cinnamon.ScreenSaver')
             self.session_bus.remove_signal_receiver(self.playPause, 'ActiveChanged', 'org.freedesktop.ScreenSaver')
+            self.session_bus.remove_signal_receiver(self.pause, 'Locked', 'com.canonical.Unity.Session')
+            self.session_bus.remove_signal_receiver(self.play, 'Unlocked', 'com.canonical.Unity.Session')
             return True
         except dbus.DBusException:
             return False
-            
-    
+
+    def play(self):
+        self.playPause(False)
+
+    def pause(self):
+        self.playPause(True)
+
     def playPause(self,state):
         if not state:
             if self.wasplaying:
