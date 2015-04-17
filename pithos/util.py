46e3b6c91154ce2e4e8f832b04324460005dd4f1
Glenn Moss <gim@keynetics.com>
2015-04-17 16:55:01 -0600

    Plugin finding architecture.
    Using pkgutil and importlib to import all modules found in a given
    namespace package and then find all sublasses of a given base class.
    
    In order to allow namespace packages under pithos/ to work properly
    pithos itself is converted to a namespace packge by removing the
    __init__.py file.

--- pithos/util.py
+++ pithos/util.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8; tab-width: 4; indent-tabs-mode: nil; -*-
 ### BEGIN LICENSE
 # Copyright (C) 2010-2012 Kevin Mehall <km@kevinmehall.net>
-#This program is free software: you can redistribute it and/or modify it 
-#under the terms of the GNU General Public License version 3, as published 
+#This program is free software: you can redistribute it and/or modify it
+#under the terms of the GNU General Public License version 3, as published
 #by the Free Software Foundation.
 #
-#This program is distributed in the hope that it will be useful, but 
-#WITHOUT ANY WARRANTY; without even the implied warranties of 
-#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#This program is distributed in the hope that it will be useful, but
+#WITHOUT ANY WARRANTY; without even the implied warranties of
+#MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 #PURPOSE.  See the GNU General Public License for more details.
 #
-#You should have received a copy of the GNU General Public License along 
+#You should have received a copy of the GNU General Public License along
 #with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
-import logging
-import webbrowser
 from urllib.parse import splittype, splituser, splitpasswd
+import importlib
+import logging
+import pkgutil
+import webbrowser
 
 def parse_proxy(proxy):
     """ _parse_proxy from urllib """
@@ -50,3 +52,24 @@ def open_browser(url):
             os.wait() # workaround for http://bugs.python.org/issue5993
         except:
             pass
+
+
+def import_all (pkg):
+    if isinstance(pkg, str):
+        pkg = importlib.import_module(pkg)
+    logging.debug("Importing all modules in package %s", pkg.__name__)
+    modules = []
+    for _, name, _ in pkgutil.iter_modules(pkg.__path__, pkg.__name__ + '.'):
+        logging.debug("Found module: %s", name)
+        try:
+            modules.append(importlib.import_module(name))
+        except Exception:
+            logging.debug("Unable to import module %s", name,
+                          exc_info=True)
+    return modules
+
+
+def find_subclasses (pkg, BaseClass):
+    import_all(pkg)
+    return {Subclass.__qualname__: Subclass
+            for Subclass in BaseClass.__subclasses__()}
