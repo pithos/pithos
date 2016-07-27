24b14b4312713e3a689c6aebf4840155070709c1
Jason Gray <jasonlevigray3@gmail.com>
2016-07-27 17:22:44 -0500

    mpris: Fix possible exception
    Fixes: TypeError: unsupported operand type(s) for //: 'NoneType' and 'int'
    
    For some reason self.window.query_duration() passes the None check and then
    shortly after returns None. This should fix that by caching the 1st call and
    reusing the value instead of calling it twice.
    
    Closes #320

--- pithos/plugins/_mpris.py
+++ pithos/plugins/_mpris.py
@@ -147,8 +147,9 @@ class PithosMprisService(DBusServiceObject):
     def _duration(self):
         # use the duration provided by Pandora
         # if Gstreamer hasn't figured out the duration yet
-        if self.window.query_duration() is not None:
-            return self.window.query_duration() // 1000
+        duration = self.window.query_duration()
+        if duration is not None:
+            return duration // 1000
         else:
             return self.window.current_song.trackLength * 1000000
 
