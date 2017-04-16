5ec07c102e25a4c8f6be4f6c7d0b902fc1f7779e
Patrick Griffis <tingping@tingping.se>
2017-04-15 22:00:20 -0400

    mediakeys: Try using org.gnome.SettingsDaemon.MediaKeys name
    This is actually correct to the documentation:
    https://github.com/GNOME/gnome-settings-daemon/blob/master/plugins/media-keys/README.media-keys-API
    
    gnome-settings-daemon 3.24 splits out plugins and the old bus
    name will no longer work. For now both are actually broken...:
    
    https://bugzilla.gnome.org/show_bug.cgi?id=781326

--- pithos/plugins/mediakeys.py
+++ pithos/plugins/mediakeys.py
@@ -49,22 +49,26 @@ class MediaKeyPlugin(PithosPlugin):
                 logging.warning(e)
                 return False
 
-            for de in ('gnome', 'mate'):
-                try:
-                    self.mediakeys = Gio.DBusProxy.new_sync(
-                        bus,
-                        Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
-                        None,
-                        'org.{}.SettingsDaemon'.format(de),
-                        '/org/{}/SettingsDaemon/MediaKeys'.format(de),
-                        'org.{}.SettingsDaemon.MediaKeys'.format(de),
-                        None,
-                    )
-                    if grab_media_keys():
-                        bound = True
-                        break
-                except GLib.Error as e:
-                    logging.warning(e)
+            for de, bus_names in {
+                'gnome': ('org.gnome.SettingDaemon.MediaKeys', 'org.gnome.SettingsDaemon'),
+                'mate': ('org.mate.SettingsDaemon', )
+            }.items():
+                for bus_name in bus_names:
+                    try:
+                        self.mediakeys = Gio.DBusProxy.new_sync(
+                            bus,
+                            Gio.DBusProxyFlags.DO_NOT_LOAD_PROPERTIES,
+                            None,
+                            bus_name,
+                            '/org/{}/SettingsDaemon/MediaKeys'.format(de),
+                            'org.{}.SettingsDaemon.MediaKeys'.format(de),
+                            None,
+                        )
+                        if grab_media_keys():
+                            bound = True
+                            break
+                    except GLib.Error as e:
+                        logging.warning(e)
         return bound
 
         def update_focus_time(widget, event, userdata=None):
