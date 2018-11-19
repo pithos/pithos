ce16a93f5bce5e557faded2ed5eb9e353bb9ef86
Patrick Griffis <tingping@tingping.se>
2018-11-19 12:07:01 -0500

    Cleanup unused imports

--- pithos/plugins/screensaver_pause.py
+++ pithos/plugins/screensaver_pause.py
@@ -12,9 +12,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
-
-from gi.repository import Gio, Gtk
+from gi.repository import Gio
 
 from pithos.plugin import PithosPlugin
 
